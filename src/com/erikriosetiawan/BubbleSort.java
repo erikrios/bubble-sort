06f0c09ee17e750a0a0aff89a6ba7b8c582d30af
erikrios <erikriosetiawan49@gmail.com>
2020-01-07 20:54:01 +0700

    Add a method to sort the values of array

--- src/com/erikriosetiawan/BubbleSort.java
+++ src/com/erikriosetiawan/BubbleSort.java
@@ -3,7 +3,7 @@ package com.erikriosetiawan;
 public class BubbleSort {
 
     // Method to sort the random values in array
-    public void sort(int[] values, int arrayLength) {
+    public static void sort(int[] values, int arrayLength) {
         for (int i = 0; i < arrayLength - 1; i++) {
             for (int j = 0; j < arrayLength - 1; j++) {
                 if (values[j] > values[j + 1]) {
@@ -14,7 +14,7 @@ public class BubbleSort {
     }
 
     // Method to swap two value
-    private void swap(int a, int b) {
+    private static void swap(int a, int b) {
         int temp = a;
         a = b;
         b = temp;
